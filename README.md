1d0b7d14fbb9ea83dca9496759ef7822936ee18f
Marc Abramowitz <abramowi@adobe.com>
2021-12-17 13:37:28 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# react-spectrum-parcel-starter
+
+This is an example repo to show how to integrate [React Spectrum][] into a
+React project with [Parcel][] as the bundler.
+
+## Screenshot
+
+![Screenshot](images/screenshot.png)
+
+## Get started
+
+```sh
+brew install gh
+gh repo clone msabramo/react-spectrum-parcel-starter
+cd react-spectrum-parcel-starter
+yarn
+PORT=1234 yarn dev
+```
+
+[react spectrum]: https://react-spectrum.adobe.com/react-spectrum/
+[parcel]: https://parceljs.org/
---
+++ src/App.tsx
@@ -0,0 +1,44 @@
+import {
+  ActionButton, Button, ButtonGroup,
+  Heading, Item,
+  Menu, MenuTrigger,
+  Provider,
+  defaultTheme,
+} from '@adobe/react-spectrum';
+
+import classes from './App.module.css';
+
+const handlePress = (str) => {
+  alert(`You pressed: ${str}`);
+};
+
+export function App() {
+  return (
+    <Provider theme={defaultTheme}>
+      <div className={classes.container}>
+        <div className={classes.container}>
+          <Heading level={1}>Hello React Spectrum!</Heading>
+
+          <Heading level={2}>Buttons</Heading>
+          <ButtonGroup>
+            <Button variant="cta" onPress={() => handlePress('Button')}>Button (cta)</Button>
+            <Button variant="primary" onPress={() => handlePress('Button')}>Button (primary)</Button>
+            <Button variant="secondary" onPress={() => handlePress('Button')}>Button (secondary)</Button>
+            <Button variant="negative" onPress={() => handlePress('Button')}>Button (negative)</Button>
+          </ButtonGroup>
+
+          <Heading level={2}>Menu</Heading>
+          <MenuTrigger>
+            <ActionButton>Edit</ActionButton>
+            <Menu onAction={(key) => alert(key)}>
+              <Item key="cut">Cut</Item>
+              <Item key="copy">Copy</Item>
+              <Item key="paste">Paste</Item>
+              <Item key="replace">Replace</Item>
+            </Menu>
+          </MenuTrigger>
+        </div>
+      </div>
+    </Provider>
+  );
+}
