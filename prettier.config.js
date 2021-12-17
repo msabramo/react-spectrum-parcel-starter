4f263b1a94d393e066bc5d9fab1c17678492318a
Marc Abramowitz <abramowi@adobe.com>
2021-12-17 14:09:43 -0800

    Add prettier.config.js

---
+++ prettier.config.js
@@ -0,0 +1,12 @@
+const config = {
+    printWidth: 80,
+    tabWidth: 2,
+    useTabs: false,
+    singleQuote: false,
+    quoteProps: "consistent",
+    jsxSingleQuote: false,
+    trailingComma: "all",
+    endOfLine: "lf",
+};
+
+module.exports = config;
