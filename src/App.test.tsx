529d2cc2172f2047d7d0b84863ffd3cc8a270e93
Marc Abramowitz <abramowi@adobe.com>
2021-12-17 15:42:34 -0800

    Reformat App.test.tsx

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -3,11 +3,11 @@ import { render, screen } from "@testing-library/react";
 import { App } from "./App";
 
 const text = {
-    sample: "Welcome to React Spectrum!",
+  sample: "Welcome to React Spectrum!",
 };
 
 // TODO: add proper tests for App component
 test("App renders properly", () => {
-    render(<App />);
-    expect(screen.getByText(text.sample)).toBeInTheDocument();
+  render(<App />);
+  expect(screen.getByText(text.sample)).toBeInTheDocument();
 });
